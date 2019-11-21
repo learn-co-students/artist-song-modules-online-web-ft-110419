d134c0bd6c07ca69f43d04303cc598d9fa832098
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 07:03:10 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,11 @@
 require 'pry'
 
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
+
+# puts Song.Memorable
+# puts Artist.Memorable
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
