ad9b9a848dd1e2e1737e405faf491add635472a2
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-22 20:17:13 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 require 'pry'
 
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
+# Require my Modules
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
 
+# Require my classes
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
